2e386ccaa563cf6ab69e78b239d11107ee2f0362
zemail <artem.zimin02@gmail.com>
2024-03-22 18:49:41 +0300

    update vue.config.js && deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'testovoe-form-medods'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/form-medods/'
+  : '/'
 })
