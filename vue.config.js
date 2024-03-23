7fd47cc09f093f746cddcdd4610efa7f60e3ddfa
zemail <artem.zimin02@gmail.com>
2024-03-23 13:28:17 +0300

    fix: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/form-medods/'
-  : '/'
+  publicPath: 'form-medods'
 })
